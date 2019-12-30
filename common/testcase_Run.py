8bc16019635de275ceb676cf76cf15bcfbcb5992
Jerry Huang <jerrhuang@nvidia.com>
2019-12-30 16:29:05 +0800

    add log analyze

--- common/testcase_Run.py
+++ common/testcase_Run.py
@@ -3,56 +3,131 @@ import os
 import io
 import datetime
 import time
+import re
 from common import testcase_Parse
+from common import logAnalyze
 
-
-def run_testCase(caseName, curan_path, arguementList):
-    caseCmds = testcase_Parse.gettestcaseCmdList("single", caseName)
- #   print("caseCmds:{}".format(caseCmds))
+def run_testcase(caseName, curan_path, arguementList):
     createFolder('logs')
-    runSinglecase(caseCmds, curan_path, arguementList)
+    if caseName == 'all_case' or caseName == 'all':
+        run_allTestcase(caseName, curan_path, arguementList)
+    else:
+        run_singleTestcase(caseName, curan_path, arguementList)
 
-def runSinglecase(caseCmds, curan_path, arguementList):
-    logname = caseCmds[0].strip().replace('\n', '').strip().split(':')[-1].strip()
-    logname = '-'.join([logname, getcurrDate()])
+def run_singleTestcase(caseName, curan_path, arguementList):
+    caseCmds = testcase_Parse.gettestcaseCmdList(caseName)
 
-    tempfile = 'log.txt'
+    runCases(caseCmds, curan_path, arguementList)
 
+def runCases(caseCmds, curan_path, arguementList):
+    print('casecmd={}'.format(caseCmds))
+    casename = caseCmds[0].strip().replace('\n', '').strip().split(':')[-1].strip()
+    suitename = caseCmds[0][5:]
+
+    logname = '-'.join([casename, getcurrDate()])
+
+    tempfile = 'logs/log.txt'
+    if checkFileStatus(tempfile):
+        os.remove(tempfile)
+
+    for cmd in caseCmds:
+        if '#' in cmd or cmd.strip() == '':
+            continue
+        elif suitename.split(':')[0].strip() == 'cuPHY_PUSCH_LDPC_support_multiple_code_rates_including_HARQ_rate':
+            runParticularCase_cuPHY_PUSCH_LDPC_support_multiple_code_rates_including_HARQ_rate(cmd, suitename, curan_path, tempfile, logname, arguementList.duration, arguementList.iter)
+        else:
+            cmd = convertCmdToAbspath(cmd, curan_path)
+            runCommandsAndSaveLog(cmd, arguementList.iter, arguementList.duration, tempfile, logname, suitename)
+            #runCommandsAndSaveLog(cmd, arguementList.iter, arguementList.duration, tempfile, logname, suitename)
+
+    """
     if arguementList.duration == 0:
         for cmd in caseCmds:
-            if '#####' in cmd or cmd.strip() == '':
+            if '#' in cmd or cmd.strip() == '':
                 continue
             else:
-                cmd = convertCmdToAbspath(cmd, curan_path)
-                for i in range(arguementList.iter):
-                    runCommand(cmd, tempfile)
-                    writelog(tempfile, 'logs/%s.txt' % logname, cmd)
-                    os.remove(tempfile)
+                if suitename.split(':')[0].strip() == 'cuPHY_PUSCH_LDPC_support_multiple_code_rates_including_HARQ_rate':
+                    runParticularCase_cuPHY_PUSCH_LDPC_support_multiple_code_rates_including_HARQ_rate(cmd, suitename, curan_path, tempfile, logname, arguementList.iter)
+                else:
+                    cmd = convertCmdToAbspath(cmd, curan_path)
+                    runCommandsAndSaveLog(cmd, arguementList.iter, tempfile, logname, suitename)
     else:
+        for cmd in caseCmds:
+            if '#' in cmd or cmd.strip() == '':
+                continue
+            print("AAAAAAA={}".format(cmd))
+            starttime = time.time()
+            while ((time.time()-starttime)/60) < arguementList.duration:
+                if suitename.split(':')[0].strip() == 'cuPHY_PUSCH_LDPC_support_multiple_code_rates_including_HARQ_rate':
+                    runParticularCase_cuPHY_PUSCH_LDPC_support_multiple_code_rates_including_HARQ_rate(cmd, suitename, curan_path, tempfile, logname)
+                else:
+                    cmd = convertCmdToAbspath(cmd, curan_path)
+                    runCommandsAndSaveLog(cmd, arguementList.iter, tempfile, logname, suitename)
+    """
+    pass
+    """
         starttime = time.time()
         while ((time.time()-starttime)/60) < arguementList.duration:
             for cmd in caseCmds:
-                if '#####' in cmd or cmd.strip() == '':
+                if '#' in cmd or cmd.strip() == '':
                     continue
                 else:
                     cmd = convertCmdToAbspath(cmd, curan_path)
                     runCommand(cmd, tempfile)
-                    writelog(tempfile, 'logs/%s.txt' % logname, cmd)
+                    writelog(tempfile, 'logs/%s.txt' % logname, cmd, suitename)
                     os.remove(tempfile)
+    """
+
+def runCommandsAndSaveLog(cmd, iter_times, duration, temp_file, logname, suitename):
+    if (duration == 0):
+        print('run case {} times:'.format(iter_times))
+        print('{}'.format(cmd))
+        loopRunCommand_byiter(cmd, iter_times, temp_file)
+    else:
+        print('run case in {} minutes:'.format(duration))
+        print('{}'.format(cmd))
+        loopRunCommand_bytime(cmd, duration, temp_file)
+
+    logAnalyze.checkResultBycase(temp_file, suitename.split(':')[0].strip())
+
+    writelog(temp_file, 'logs/%s.txt' % logname, cmd, suitename)
+    os.remove(temp_file)
+
+def loopRunCommand_byiter(cmd, iter_times, temp_file):
+    for i in range(iter_times):
+        runCommand(cmd, temp_file)
+
+def loopRunCommand_bytime(cmd, duration, temp_file):
+    starttime = time.time()
+    while ((time.time()-starttime)/60) < duration:
+        runCommand(cmd, temp_file)
 
 def runCommand(cmd, logfile):
     if 'sudo' in cmd:
         password = 'dgxqa'
-        os.system('echo %s | sudo -S %s > %s' % (password, cmd, logfile))
+        os.system('echo %s | sudo -S %s >> %s' % (password, cmd, logfile))
     else:
-        os.system('%s > %s' % (cmd, logfile))
+        os.system('%s >> %s' % (cmd, logfile))
 
 def convertCmdToAbspath(cmd, curan_path):
     cmd = cmd.strip().replace('\n', '')
     cmd = cmd.replace('cuPHY/build/', '/'.join([curan_path, 'cuPHY/build/']))
     cmd = cmd.replace('./testVectors', '/'.join([curan_path,'testVectors']))
-    print('convertCmdToAbspath={}'.format(cmd))
+
     return cmd
+
+def runParticularCase_cuPHY_PUSCH_LDPC_support_multiple_code_rates_including_HARQ_rate(cmd, suitename, curan_path, tempfile, logname, duration = 0, iter_times=1):
+    match = re.compile(r'\d+~\d+', re.DOTALL)
+    pValue = match.findall(cmd)[0].split('~')
+    pMin, pMax = int(pValue[0]), int(pValue[1])
+
+    for i in range(pMin, pMax+1, 1):
+        newCmd = re.sub(match, str(i), cmd)
+        newCmd = convertCmdToAbspath(newCmd, curan_path)
+        runCommandsAndSaveLog(newCmd, iter_times, duration, tempfile, logname, suitename)
+
+def runOneCaseAndAnalyze(cmd, curan_path):
+    pass      
 
 def createFolder(folder):
     currenWorkPath = os.getcwd()
@@ -83,27 +158,32 @@ def createFile(filename):
     else:
         pass
 
+def checkFileStatus(filename):
+    return True if os.path.isfile(filename) else False
+
 def copyLog(srcfile, dstfile):
     with open(srcfile, 'r') as srcfh:
         with open(dstfile, 'a+') as dstfh:
             dstfh.write(srcfh.read())
             dstfh.write('\n')
 
-def writelog(srcfile, dstfile, header):
+def writelog(srcfile, dstfile, cmds, suitename):
     with open(dstfile, 'a+') as f:
-        f.write(header + '\n')
+        f.write(cmds + '\n')
         f.write('*****start time {}'.format(getcurrDate()) + '\n')
+        f.write('*****run {}'.format(suitename) + '\n')
     copyLog(srcfile, dstfile)
     with open(dstfile, 'a+') as f:
         f.write('*****End time {}'.format(getcurrDate()))
         f.write('\n\n')
 
-def run_alltestcases(caseName, curan_path, iter=1, time=0):
-    caseCmds = testcase_Parse.gettestcaseCmdList("single", caseName)
+def run_allTestcase(caseName, curan_path, arguementList):
+    caseCmds = testcase_Parse.gettestcaseCmdList(caseName)
 
     caselist = seperateTestcase(caseCmds)
+
     for i, case in enumerate(caselist):
-        runSinglecase(case, curan_path)
+        runCases(case, curan_path, arguementList)
 
 def seperateTestcase(allcases):
     caseNamePos = []
--- run.py
+++ run.py
@@ -10,11 +10,10 @@ if __name__ == '__main__':
     print("args:{}".format(args))
 
     if args.case:
-        print("run the case")
-        testcase_Run.run_testCase(args.case[0], args.curan[0], args)
+        testcase_Run.run_testcase(args.case[0], args.curan[0], args)
     elif args.all_case:
         print("run all cases")
-        testcase_Run.run_alltestcases("all_case", args.curan[0], args)
+        testcase_Run.run_allTestcase("all_case", args.curan[0], args)
     elif args.analyze:
         print("analyze the log")
 
